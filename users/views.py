a8671926fa699f692fe4fe600fca78479c41ff92
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-01 15:02:24 +0300

    controllers for Payment create and list

--- users/views.py
+++ users/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-
-from users.models import User
-from users.serializers import UserSerializer
+from rest_framework import viewsets, generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializers
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    serializer_class = PaymentSerializers
+    queryset = Payment.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializers
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson',)
+    ordering_fields = ('date',)
