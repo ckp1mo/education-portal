cc188f048d0c1e7d6228973525e7172a1e250486
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-01 15:00:52 +0300

    simple serializers for model Payment

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Payment
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class PaymentSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
         fields = '__all__'
