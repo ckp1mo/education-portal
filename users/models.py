b0926fb697a76f9ef3c724f7596ab10029181f4e
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-01 14:57:48 +0300

    new model for Payment

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-NULLABLE = {'blank': True, 'null': True}
+from lms.models import Lesson, Course, NULLABLE
 
 
 class User(AbstractUser):
@@ -20,3 +19,24 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Payment(models.Model):
+    payment_variant = [
+        ('Наличные', 'cash'),
+        ('Картой', 'card'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    date = models.DateField(auto_now=True, verbose_name='Дата платежа')
+    lesson = models.ForeignKey(Lesson, related_name='lesson', on_delete=models.CASCADE, verbose_name='Урок', **NULLABLE)
+    course = models.ForeignKey(Course, related_name='course', on_delete=models.CASCADE, verbose_name='Курс', **NULLABLE)
+    payment = models.PositiveIntegerField(verbose_name='Сумма платежа')
+    payment_method = models.CharField(max_length=30, choices=payment_variant, verbose_name='Способ оплаты')
+
+    def __str__(self):
+        return f'{self.user}, {self.lesson if self.lesson else self.course}, {self.date}'
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
