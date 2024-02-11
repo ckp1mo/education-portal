58ab3b0d684e61198f8654034aea77d8b4ff6faa
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-11 21:57:14 +0300

    working with permissions.

--- lms/views.py
+++ lms/views.py
@@ -1,37 +1,74 @@
-from django.shortcuts import render
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from lms.models import Course, Lesson
+from lms.permissions import IsModerator, IsOwner
 from lms.serializers import CourseSerializer, LessonSerializer
+from users.models import UserRole
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset()
+        if self.request.user.role == UserRole.MODERATOR:
+            return queryset
+        else:
+            return queryset.filter(owner=self.request.user)
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        elif self.action == 'destroy':
+            self.permission_classes = [IsAuthenticated, ~IsModerator]
+        return [permission() for permission in self.permission_classes]
+
+    def perform_create(self, serializer):
+        new_course = serializer.save()
+        new_course.owner = self.request.user
+        new_course.save()
+
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, ~IsModerator]
+
+    def perform_create(self, serializer):
+        new_lesson = serializer.save()
+        new_lesson.owner = self.request.user
+        new_lesson.save()
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset()
+        if self.request.user.role == UserRole.MODERATOR:
+            return queryset
+        else:
+            return queryset.filter(owner=self.request.user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonDeleteAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-
+    permission_classes = [IsAuthenticated, IsOwner, ~IsModerator]
 
