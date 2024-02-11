64167b5603f906f0e629dfbeca68eb78735eb64d
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-11 21:53:23 +0300

    added fileds "owner" to models

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
@@ -7,6 +8,7 @@ class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     preview = models.ImageField(upload_to='main_app/', verbose_name='картинка', **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
@@ -22,6 +24,7 @@ class Lesson(models.Model):
     preview = models.ImageField(upload_to='main_app/', verbose_name='картинка', **NULLABLE)
     video_link = models.CharField(max_length=150, verbose_name='Ссылка на видео', **NULLABLE)
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name='Уроки', **NULLABLE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
