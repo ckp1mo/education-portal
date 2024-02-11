cd98c0642558a7e0dcec02eb116e139563d32e1b
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-11 21:55:59 +0300

    added path

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lms.urls', namespace='lms')),
-    path('', include('users.urls', namespace='users')),
+    path('user/', include('users.urls', namespace='users')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from users.apps import UsersConfig
-from users.views import UserViewSet, PaymentCreateAPIView, PaymentListAPIView
+from users.views import PaymentCreateAPIView, PaymentListAPIView, UserCreateAPIView, UserListAPIView, \
+    UserRetrieveAPIView, UserUpdateAPIView, UserDeleteAPIView
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
-
 urlpatterns = [
-    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment-create'),
-    path('payment/list/', PaymentListAPIView.as_view(), name='payment-list')
-] + router.urls
+    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment_create'),
+    path('payment/list/', PaymentListAPIView.as_view(), name='payment_list'),
+    # авторизация
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # user CRUD
+    path('create/', UserCreateAPIView.as_view(), name='user_create'),
+    path('list/', UserListAPIView.as_view(), name='user_list'),
+    path('retrieve/<int:pk>', UserRetrieveAPIView.as_view(), name='user_retrieve'),
+    path('update/<int:pk>', UserUpdateAPIView.as_view(), name='user_update'),
+    path('delete/<int:pk>', UserDeleteAPIView.as_view(), name='user_delete'),
+]
