1ea728c6cd6df94f667ad1ad53cb6ffb7dda093c
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-11 21:56:32 +0300

    CRUD for model User

--- users/views.py
+++ users/views.py
@@ -1,13 +1,37 @@
-from rest_framework import viewsets, generics
+from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializers
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class PaymentCreateAPIView(generics.CreateAPIView):
