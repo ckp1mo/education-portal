1b2f700de3cf0829243c31e1dccf3726d5fc91ac
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-18 22:27:16 +0300

    added field 'sub' in CourseSerializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,21 +1,33 @@
 from rest_framework import serializers
 
 from lms.models import Course, Lesson
+from lms.validators import VideoLinkValidator
+from users.models import Subscription
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [VideoLinkValidator(field='video_link')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(source='lesson_set', read_only=True, many=True)
+    subscription = serializers.SerializerMethodField()
+
+    def get_subscription(self, obj):
+        request = self.context.get('request')
+        if request:
+            return Subscription.objects.filter(user=request.user, course=obj).exists()
+        return False
+
+    def get_lesson_count(self, instance):
+        return instance.lesson_set.count()
 
     class Meta:
         model = Course
         fields = '__all__'
 
-    def get_lesson_count(self, instance):
-        return instance.lesson_set.count()
