0ecaf5991b70fc02bae5714e71d7162d28bbd136
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-18 22:24:03 +0300

    url for sub

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
 )
 from users.apps import UsersConfig
 from users.views import PaymentCreateAPIView, PaymentListAPIView, UserCreateAPIView, UserListAPIView, \
-    UserRetrieveAPIView, UserUpdateAPIView, UserDeleteAPIView
+    UserRetrieveAPIView, UserUpdateAPIView, UserDeleteAPIView, SubscriptionCreateAPIView, SubscriptionDestroyAPIView
 
 app_name = UsersConfig.name
 
@@ -21,4 +21,7 @@ urlpatterns = [
     path('retrieve/<int:pk>', UserRetrieveAPIView.as_view(), name='user_retrieve'),
     path('update/<int:pk>', UserUpdateAPIView.as_view(), name='user_update'),
     path('delete/<int:pk>', UserDeleteAPIView.as_view(), name='user_delete'),
+    # подписка
+    path('subscription_create/', SubscriptionCreateAPIView.as_view(), name='sub_create'),
+    path('subscription_delete/<int:pk>', SubscriptionDestroyAPIView.as_view(), name='sub_delete'),
 ]
