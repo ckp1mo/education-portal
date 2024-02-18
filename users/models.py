e9ee63d68f4a26346aed03f578a5454527cc6ece
Alexandr Gritsunov <ickpimo@gmail.com>
2024-02-18 22:09:29 +0300

    create model for Subscription

--- users/models.py
+++ users/models.py
@@ -45,3 +45,15 @@ class Payment(models.Model):
     class Meta:
         verbose_name = 'Платеж'
         verbose_name_plural = 'Платежи'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.user}, {self.course}'
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
