b1368a82806ea6719147ccc054dfbf76c6577f00
Alexandr Gritsunov <ickpimo@gmail.com>
2024-03-20 16:59:40 +0300

    renamed variables

--- config/settings.py
+++ config/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'db',
     }
 }
